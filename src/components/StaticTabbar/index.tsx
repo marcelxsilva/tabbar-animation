34d4c9ec214e7e2af39354e6a8f47c901044772a
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-02-20 05:32:02 -0300

    apply event to up and down icon when clicked

--- src/components/StaticTabbar/index.tsx
+++ src/components/StaticTabbar/index.tsx
@@ -12,14 +12,33 @@ export const tabHeight = 70;
 const { width } = Dimensions.get('window');
 
 export default class StaticTabbar extends Component<Props> {
-
+  values: Animated.Value[] = []
+  constructor(props: Props) {
+    super(props);
+    const { tabs } = this.props;
+    this.values = tabs.map((tab, index) => new Animated.Value(index === 0 ? 1 : 0))
+  }
   onPress = (index: number) => {
     const { value, tabs } = this.props;
     const tabWidth = width / tabs.length;
-    Animated.spring(value, {
-      toValue: -width + tabWidth * index,
-      useNativeDriver: true,
-    }).start()
+    Animated.sequence([
+      ...this.values.map(value => Animated.timing(value, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: true,
+      })),
+
+      Animated.parallel([
+        Animated.spring(this.values[index], {
+          toValue: 1,
+          useNativeDriver: true,
+        }),
+        Animated.spring(value, {
+          toValue: -width + tabWidth * index,
+          useNativeDriver: true,
+        })
+      ]),
+    ]).start()
   }
 
   render() {
@@ -29,6 +48,8 @@ export default class StaticTabbar extends Component<Props> {
       <View style={styles.container}>
         {
           tabs.map(({ name }, index) => {
+            const activeValue = this.values[index];
+
             const opacity = value.interpolate({
               inputRange: [
                 -width + tabWidth * (index - 1),
@@ -37,18 +58,41 @@ export default class StaticTabbar extends Component<Props> {
               ],
               outputRange: [1, 0, 1],
               extrapolate: 'clamp'
+            });
+
+            const translateY = activeValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [tabHeight, 0]
             })
 
             return (
-              <TouchableWithoutFeedback
-                key={index}
-                onPress={() => this.onPress(index)}
-                {...{ index }}
-              >
-                <Animated.View style={[styles.tab, { opacity }]}>
-                  <Icon size={25} {...{ name }} />
+              <React.Fragment key={index}>
+                <TouchableWithoutFeedback
+                  key={index}
+                  onPress={() => this.onPress(index)}
+
+                >
+                  <Animated.View style={[styles.tab, { opacity }]}>
+                    <Icon size={25} {...{ name }} />
+                  </Animated.View>
+                </TouchableWithoutFeedback>
+
+                <Animated.View
+                  style={{
+                    position: 'absolute',
+                    top: -8,
+                    width: tabWidth,
+                    left: tabWidth * index,
+                    height: tabHeight,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    transform: [{ translateY }]
+                  }}>
+                  <View style={styles.circle}>
+                    <Icon size={25} {...{ name }} />
+                  </View>
                 </Animated.View>
-              </TouchableWithoutFeedback>
+              </React.Fragment>
             )
           })
         }
@@ -63,6 +107,14 @@ const styles = StyleSheet.create({
   tab: {
     flex: 1,
     height: tabHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   }
