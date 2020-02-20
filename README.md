47b897c02a1f93d5ad2da5b080d288c348d7d88e
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-02-20 03:57:17 -0300

    created tab bar bottom

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">Welcome to Tab Bar Animation Challenge 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1-blue.svg?cacheSeconds=2592000" />
+  <a href="https://twitter.com/marcelxsilva" target="_blank">
+    <img alt="Twitter: marcelxsilva" src="https://img.shields.io/twitter/follow/marcelxsilva.svg?style=social" />
+  </a>
+</p>
+
+ 
+## Install
+
+```sh
+yarn install
+```
+
+## Usage
+
+```sh
+yarn start
+```
+
+
+
+### Tecnologias Utilizadas
+  - Expo
+  - d3-shape
+
+
+## Author
+
+> **Marcelo Silva**
+
+* Twitter: [@marcelxsilva](https://twitter.com/marcelxsilva)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
--- src/components/Tabbar/index.tsx
+++ src/components/Tabbar/index.tsx
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView, Dimensions, StyleSheet, Animated } from 'react-native';
+import * as shape from "d3-shape";
+import Svg, { Path } from 'react-native-svg';
+import StaticTabbar from '../StaticTabbar';
 
-// import { Container } from './styles';
+const { width } = Dimensions.get('window');
+const height = 70;
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+const tabs = [
+  { name: 'grid' },
+  { name: 'list' },
+  { name: 'refresh-cw' },
+  { name: 'box' },
+  { name: 'user' },
+];
 
-interface Props {}
+const tabWidth = width / tabs.length;
+const left = shape.line()
+  .x(d => d.x)
+  .y(d => d.y)
+  .curve(shape.curveBasis)([
+    { x: 0, y: 0 },
+    { x: width, y: 0 },
+  ])
+
+
+const getPath = (): string => {
+  const left = shape.line().x(d => d.x).y(d => d.y)([
+    { x: 0, y: 0 },
+    { x: width, y: 0 },
+  ]);
+  const tab = shape.line()
+    .x(d => d.x)
+    .y(d => d.y)
+    .curve(shape.curveBasis)([
+      { x: width, y: 0 },
+      { x: width + 5, y: 0 },
+      { x: width + 10, y: 10 },
+      { x: width + 10, y: height },
+      { x: width + tabWidth - 10, y: height },
+      { x: width + tabWidth - 10, y: 10 },
+      { x: width + tabWidth - 5, y: 0 },
+      { x: width + tabWidth, y: 0 },
+    ]);
+  const right = shape.line().x(d => d.x).y(d => d.y)([
+    { x: width + tabWidth, y: 0 },
+    { x: width * 2, y: 0 },
+    { x: width * 2, y: height },
+    { x: 0, y: height },
+    { x: 0, y: 0 },
+  ]);
+  return `${left} ${tab} ${right}`;
+};
+const d = getPath();
+
+interface Props { }
 
 export default class Tabbar extends Component<Props> {
-    render() {
-        return <View />;
-    }
+  render() {
+    return (
+      <>
+        <View {...{ width, height }}>
+          <AnimatedSvg width={width * 2} {...{ height }} >
+            <Path {...{ d }} fill='white' />
+            <SafeAreaView style={styles.safeArea} />
+          </AnimatedSvg>
+          <View style={styles.absoluteFill}>
+            <StaticTabbar {...{ tabs }} />
+          </View>
+        </View>
+      </>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#fff'
+  },
+  absoluteFill: {
+
+  }
+})
