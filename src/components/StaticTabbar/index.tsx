32b39816c3fe3deb819dda507918d4504d1755b4
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-02-20 05:03:03 -0300

    handled style to show only active

--- src/components/StaticTabbar/index.tsx
+++ src/components/StaticTabbar/index.tsx
@@ -12,30 +12,45 @@ export const tabHeight = 70;
 const { width } = Dimensions.get('window');
 
 export default class StaticTabbar extends Component<Props> {
+
   onPress = (index: number) => {
     const { value, tabs } = this.props;
     const tabWidth = width / tabs.length;
-
     Animated.spring(value, {
       toValue: -width + tabWidth * index,
       useNativeDriver: true,
     }).start()
   }
+
   render() {
-    const { tabs } = this.props;
+    const { tabs, value } = this.props;
+    const tabWidth = width / tabs.length;
     return (
       <View style={styles.container}>
         {
-          tabs.map(({ name }, index) => (
-            <TouchableWithoutFeedback
-              key={index}
-              onPress={() => this.onPress(index)} {...{ index }}
-            >
-              <View style={styles.tab}>
-                <Icon size={25} {...{ name }} />
-              </View>
-            </TouchableWithoutFeedback>
-          ))
+          tabs.map(({ name }, index) => {
+            const opacity = value.interpolate({
+              inputRange: [
+                -width + tabWidth * (index - 1),
+                -width + tabWidth * index,
+                -width + tabWidth * (index + 1),
+              ],
+              outputRange: [1, 0, 1],
+              extrapolate: 'clamp'
+            })
+
+            return (
+              <TouchableWithoutFeedback
+                key={index}
+                onPress={() => this.onPress(index)}
+                {...{ index }}
+              >
+                <Animated.View style={[styles.tab, { opacity }]}>
+                  <Icon size={25} {...{ name }} />
+                </Animated.View>
+              </TouchableWithoutFeedback>
+            )
+          })
         }
       </View>
     )
